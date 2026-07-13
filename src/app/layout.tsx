5672d2c1bd2c9ea07b823a286357bdae8a89b18f
Eduardo Lima <138487802+Dudruu@users.noreply.github.com>
2026-07-13 11:22:59 -0300

    feat/estrutura do header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter} from "next/font/google";
 import "./globals.css";
+import Header from "../components/header";
 
 const inter = Inter({
   variable : "--font-inter",
@@ -23,6 +24,7 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${inter.variable}  antialiased`}
     >
+      <Header/>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
