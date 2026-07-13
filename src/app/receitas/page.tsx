bc33a50e121e5a53bfc828804b932c60b7cfff97
Eduardo Lima <138487802+Dudruu@users.noreply.github.com>
2026-07-13 16:34:23 -0300

    feat: responsividade aplicada

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -5,9 +5,9 @@ import { recipes } from "@/src/lib/data";
 export default function ReceitasPage() {
   return (
     <main className="grow py-8">
-      <div className="container mx-auto">
-        <h1 className="text-3xl font-bold">Todas as receitas</h1>
-        <div className="grid grid-cols-3 gap-8 mt-8"> 
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl sm:text-left font-bold">Todas as receitas</h1>
+         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8"> 
 
             {recipes.map((recipe)=>(<RecipeCard key={recipe.id} recipe={recipe}/>))}
         </div>
