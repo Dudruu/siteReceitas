1539aa8952e0180b68c873344e0fc371d8f14eb2
Eduardo Lima <138487802+Dudruu@users.noreply.github.com>
2026-07-13 13:17:28 -0300

    style: hover no botão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
           <div className="container mx-auto flex flex-col items-center  gap-6 py-12">
           <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
           <p className="text-xl ">Descubra as receitas simples e saborosas para todas as ocasiões</p>
-          <Link className="text-white bg-orange-500 px-3 py-2 font-bold rounded-lg"href="/receitas">Ver todas as receitas</Link>
+          <Link className="text-white bg-orange-500 px-3 py-2 font-bold rounded-lg hover:bg-orange-700 transition-colors"href="/receitas">Ver todas as receitas</Link>
           </div>
         </section>
       
