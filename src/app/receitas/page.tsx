152562763510fcbd0f35cd86027b07616f8dbe12
Eduardo Lima <138487802+Dudruu@users.noreply.github.com>
2026-07-28 16:49:04 -0300

    fix correção da pagina das receitas[id]

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -9,114 +9,119 @@ import type { Recipe } from "@/src/lib/data";
 import { Plus } from "lucide-react";
 import { useEffect, useState } from "react";
 
-export default function ReceitasPage() {
-  const [isRecipeModalOpen, setIsRecipeModalOpen] = useState(false);
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [modalMode, setModalMode] = useState<"create" | "edit">("create");
-  const [isDeleteConfirmationModalOPen, setIsDeleteConfirmationModalOpen] =
-    useState(false);
-  const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(
-    undefined
-  );
 
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const response = await api.get("/recipes");
-        setRecipes(response.data);
-      } catch (error) {
-        console.error("erro ao requisitar", error);
-      }
+
+export default function ReceitasPage() {  
+    const [isRecipeModalOpen, setIsRecipeModalOpen] =  useState(false);
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [isDeleteConfirmationModalOpen, setIsDeleteConfirmationModalOpen] = useState(false);
+    const [modalMode, setModalMode] = useState<"create" | "edit">("create");
+    const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined >(undefined);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const response = await api.get("/recipes");
+                setRecipes(response.data);
+            } catch (error) {
+                console.error("Erro ao buscar receitas:", error);
+               
+            }
+        };
+
+        fetchRecipes();
+    }, [])
+
+    const handleOpenCreateModal = () => {
+        setModalMode("create");
+        setSelectedRecipe(undefined);
+        setIsRecipeModalOpen(true);
     };
-    fetchRecipes();
-  }, []);
 
-  const handleOpenCreateModal = () => {
-    setModalMode("create");
-    setSelectedRecipe(undefined);
-    setIsRecipeModalOpen(true);
-  };
+    const handleOpenEditModal = (recipe: Recipe) => {
+        setModalMode("edit");
+        setSelectedRecipe(recipe);
+        setIsRecipeModalOpen(true);
+    };
 
-  const handleOpenEditModal = (recipe: Recipe) => {
-    setModalMode("edit");
-    setSelectedRecipe(recipe);
-    setIsRecipeModalOpen(true);
-  };
+    const handleCloseModal = () => {
+        setIsRecipeModalOpen(false);
+        setSelectedRecipe(undefined);
+    };
 
-  const handleCloseModal = () => {
-    setIsRecipeModalOpen(false);
-  };
+    const handleSaveRecipe = async (recipeData: Omit<Recipe, 'id'> | Recipe) => {
+        try {
+            if (modalMode === "create") {
+            const response = await api.post("/recipes", recipeData);
+            const newRecipe = response.data;
+            setRecipes((prev) => [...prev, newRecipe]);
+            
+        } else {
+            const updatedRecipe = recipeData as Recipe;
+            const response = await api.put(`/recipes/${updatedRecipe.id}`, updatedRecipe);
+            setRecipes((prev) => prev.map((recipe) => (recipe.id === updatedRecipe.id ? response.data : recipe))
+            );
+        }
+        handleCloseModal();
+        } catch (error) {
+            console.error(`Erro ao ${modalMode === "create" ? "criar" : "editar"} a receita:`, error);
+        }
+    };
 
-  const handleDeleteRecipe = () => {
-    if (selectedRecipe) {
-      setRecipes((prev) =>
-        prev.filter((recipe) => recipe.id !== selectedRecipe.id)
-      );
-      setIsDeleteConfirmationModalOpen(false);
-      setSelectedRecipe(undefined);
-    }
-  };
+    const handleOpenDeleteConfirmationModal = (recipe: Recipe) => {
+        setSelectedRecipe(recipe);
+        setIsDeleteConfirmationModalOpen(true);
+    };
 
-  const handleOpendDeleteConfirmationModal = (recipe: Recipe) => {
-    setSelectedRecipe(recipe);
-    setIsDeleteConfirmationModalOpen(true);
-  };
+    const handleDeleteRecipe = async () => {
+        try{
+            if (selectedRecipe) {
+                await api.delete(`/recipes/${selectedRecipe.id}`);
+                setRecipes((prev) => prev.filter((recipe) => recipe.id !== selectedRecipe.id));
+                setIsDeleteConfirmationModalOpen(false);
+                setSelectedRecipe(undefined);
+            }
+        } catch (error) {
+            console.error("Erro ao deletar a receita:", error);
+        }
+        
+    };
 
-  const handleSaveRecipe = async (recipeData: Omit<Recipe, "id"> | Recipe) => {
+    return (
+        <main className="flex grow py-8">
+            <div className="container mx-auto px-4">
+                <div className="flex justify-between w-full">
+                    <h1 className="text-3xl font-bold text-center sm:text-left">Todas as receitas</h1>
 
-
-  try {
-    if (modalMode === "create") {
-      const response = await api.post("/recipes", recipeData);
-      const newRecipe = response.data;
-      setRecipes((prev) => [...prev, newRecipe]);
-    } else{
-      const UpdatedRecipe = recipeData as Recipe;
-      const response = await api.put(`/recipes/${UpdatedRecipe.id}`,UpdatedRecipe)
-      setRecipes((prev)=>prev.map((recipe)=>recipe.id === UpdatedRecipe.id ? response.data : recipe))
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-  return (
-    <main className="grow py-8">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between w-full">
-          <h1 className="text-3xl sm:text-left font-bold">Todas as receitas</h1>
-          <button
-            onClick={handleOpenCreateModal}
-            className="flex gap-2 px-4 text-white items-center py-2 border rounded-lg hover:bg-gray-800 transition:colors bg-black"
-          >
-            <Plus size={16} />
-            Nova receita
-          </button>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8">
-          {recipes.map((recipe) => (
-            <RecipeCard
-              key={recipe.id}
-              onDelete={() => handleOpendDeleteConfirmationModal(recipe)}
-              recipe={recipe}
-              onEdit={() => handleOpenEditModal(recipe)}
+                    <button onClick={handleOpenCreateModal} className="flex gap-2 px-4 py-2 border text-white bg-black hover:bg-gray-800 rounded-lg items-center transition-colors">
+                        <Plus size={16}/>
+                        Nova Receita
+                    </button>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8"> 
+                    {recipes.map((recipe) => (
+                    <RecipeCard 
+                        key={recipe.id} 
+                        recipe={recipe} 
+                        onEdit={() => handleOpenEditModal(recipe)}
+                        onDelete={() => handleOpenDeleteConfirmationModal(recipe)}
+                    />
+                ))}
+                </div>
+            </div>
+            <RecipeFormModal 
+                isOpen={isRecipeModalOpen} 
+                onClose={(handleCloseModal)} 
+                onSave={handleSaveRecipe} 
+                mode={modalMode} 
+                recipe={selectedRecipe} 
             />
-          ))}
-        </div>
-      </div>
-      <RecipeFormModal
-        isOpen={isRecipeModalOpen}
-        onClose={handleCloseModal}
-        onSave={handleSaveRecipe}
-        mode={modalMode}
-        recipe={selectedRecipe}
-      />
-      <DeleteConfirmationModal
-        isOpen={isDeleteConfirmationModalOPen}
-        onClose={() => setIsDeleteConfirmationModalOpen(false)}
-        onConfirm={handleDeleteRecipe}
-        recipe={selectedRecipe}
-      />
-    </main>
-  );
+            <DeleteConfirmationModal
+                isOpen={isDeleteConfirmationModalOpen}
+                onClose={() => setIsDeleteConfirmationModalOpen(false)}
+                onConfirm={handleDeleteRecipe}
+                recipe={selectedRecipe}
+            />
+        </main>
+    );
 }
